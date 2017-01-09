2ef6ca19c25594e99c03e5f45ef4b66e0454a805
gurleensethi <sarusethi@rocketmail.com>
2017-01-10 00:10:30 +0530

    #Add : Trending users crawler

--- app/src/main/java/app/com/thetechnocafe/githubcompanion/Utilities/Constants.java
+++ app/src/main/java/app/com/thetechnocafe/githubcompanion/Utilities/Constants.java
@@ -6,4 +6,6 @@ package app.com.thetechnocafe.githubcompanion.Utilities;
 
 public class Constants {
     public static final String GITHUB_REPO_INCOMPLETE = "https://api.github.com/repos/";
+    public static final String GITHUB_TRENDING_DEVELOPERS_URL = "https://github.com/trending/developers?since=";
+    public static final String GITHUB_USER_INCOMPLETE = "https://api.github.com/users/";
 }
--- app/src/test/java/app/com/thetechnocafe/githubcompanion/ExampleUnitTest.java
+++ app/src/test/java/app/com/thetechnocafe/githubcompanion/ExampleUnitTest.java
@@ -2,6 +2,12 @@ package app.com.thetechnocafe.githubcompanion;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import app.com.thetechnocafe.githubcompanion.Models.UsersSearchModel;
+import app.com.thetechnocafe.githubcompanion.Networking.Crawlers.GitHubTrendingDevelopersCrawler;
+import app.com.thetechnocafe.githubcompanion.Utilities.Constants;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,5 +19,13 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void trendingDevelopersCrawler_isWorking() {
+        GitHubTrendingDevelopersCrawler crawler = GitHubTrendingDevelopersCrawler.getInstance();
+        List<UsersSearchModel> list = crawler.crawlAndGetUserData(Constants.GITHUB_TRENDING_DEVELOPERS_URL + "daily");
+
+        assertEquals(true, (list.size() > 10));
     }
 }
